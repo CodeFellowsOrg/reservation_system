6f53c8ed6d0a61976d1b0479090783dec45f4061
theverything <j3ffhorn@gmail.com>
2013-05-23 08:25:36 -0700

    add filter to delete reservations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,16 @@
 class Customer < ActiveRecord::Base
+  before_destroy :delete_reservations
+
   attr_accessible :name
 
   validates :name, presence: true
 
   has_many :reservations
   has_many :customers, through: :reservations
+
+
+  private
+  def delete_reservations
+    self.reservations.destroy_all
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,15 @@
 class Restaurant < ActiveRecord::Base
+  before_destroy :delete_reservations
+
   attr_accessible :name
 
   validates :name, presence: true
 
   has_many :reservations
   has_many :customers, through: :reservations
+
+  private
+  def delete_reservations
+    self.reservations.destroy_all
+  end
 end
